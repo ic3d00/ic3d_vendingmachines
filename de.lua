0a9d614e844641e60b7c782ba5b0a3fddf55d0c9
Crizii <135024567+CriziiTV@users.noreply.github.com>
2023-11-25 23:23:56 +0100

    Create de.lua
    German Language File

---
+++ de.lua
@@ -0,0 +1,6 @@
+Locales["de"] = {
+  ["nearprop"] = "Drücke ~g~[E] ~c~, um an den Automaten zu gehen",
+  ["bought"] = "Du hast %sx %s für ~r~$%s gekauft",
+  ["not_enough"] = "Du hast nicht genug Geld, es fehlt dir ~r~$%s",
+  ["player_cannot_hold"] = "Du hast nicht genug Platz in deinem Inventar!"
+}
